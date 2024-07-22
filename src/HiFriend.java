0382a432f07ac1fae9b0e56565ef2df817513b69
Patrick Xiong <PXstiegler@gmail.com>
2024-07-22 13:14:43 -0400

    1st Commit

---
+++ src/HiFriend.java
@@ -0,0 +1,8 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String greeting = "Hi";
+        String friend = " Patrick";
+        String lastName = " Xiong!";
+        System.out.println(greeting + friend + lastName);
+    }
+}
